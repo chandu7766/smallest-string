7575ce7aefcad060d1766a59adcd45f06b49fbd6
saichandugampa <gampa.saichandu95@gmail.com>
2019-09-30 11:19:42 -0500

    updated

--- javascript.js
+++ javascript.js
@@ -1,13 +1,6 @@
-// given 3 strings, find the smallest length....
-/*
-function findSmallestString(a,b,c) {
-    // get all 3 lengths
-    const i= a.length
-    const j=b.length
-    const k=c.length
-}*/
 
-function callbill(){
+
+function callvalue(){
 
     const first= document.getElementById("first").value
     const second=document.getElementById("second").value;
